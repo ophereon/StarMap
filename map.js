443f9aa1ef58a19edc99942cf4b74748698b30ce
ophereon <ophereon@outlook.com>
2017-06-14 09:54:33 +1200

    14/06-09:54

--- map.js
+++ map.js
@@ -42,16 +42,11 @@ $(document).ready(function() {
 
 	if(canvas.getContext){
 		var ctx = canvas.getContext('2d');
-
-		var cw = 1000;
-		var ch = 1000;
-		var m = 1;
-
-		var w = 0;
-		var h = 0;
+		var cw = 1000, ch = 1000;
+		var w = 0, h = 0;
+		var ox = 0, oy = 0;
 		var z = 1;
-		var ox = 0;
-		var oy = 0;
+		var m = 1;
 
 		function setup(){
 			resize();
@@ -60,7 +55,6 @@ $(document).ready(function() {
 			offsetY(0);
 			loop();
 		}
-
 		function resize(){
 			ctx.clearRect(0, 0, w, h);
 			w = canvas.width = window.innerWidth;
@@ -68,6 +62,10 @@ $(document).ready(function() {
 			var difW = cw/w;
 			var difH = ch/h;
 			m = Math.max(difW, difH);
+			draw();
+		}
+		function loop(){
+			window.requestAnimationFrame(loop);
 			draw();
 		}
 
@@ -83,7 +81,6 @@ $(document).ready(function() {
 			}
 			// console.log("l: " + level);
 		}
-
 		function offsetX(inX){
 			// console.log(inX);
 			if(inX<-(w-(w/z))) ox = -(w-(w/z));
@@ -98,14 +95,6 @@ $(document).ready(function() {
 			else oy = inY;
 			if(ch*z<h) oy += (h-(ch*z))/2;
 		}
-
-		function loop(){
-			window.requestAnimationFrame(loop);
-			draw();
-		}
-
-				// function x(inX){ return (((ox+(w/z))/2)+(inX*z)); }
-				// function y(inY){ return (((oy+(h/z))/2)+(inY*z)); }
 		function x(inX){ return ((inX+ox)*z)/m; }
 		function y(inY){ return ((inY+oy)*z)/m; }
 
@@ -113,24 +102,23 @@ $(document).ready(function() {
 			ctx.clearRect(0, 0, w, h);
 			ctx.fillStyle = "rgb(0,0,0)";
 			ctx.fillRect(0, 0, w, h);
-			// ctx.drawImage(document.getElementById('background'), 0, 0, w, h);
-			// };
-			// drawRect(0, 0, 10, 10, "rgb(255,255,255)");
-
-			for(var i=0; i<=10; ++i) {
-				drawLine(i*(cw/10), 0, i*(cw/10), ch, "rgb(50,50,50)");
+			ctx.globalAlpha = 0.5;
+			ctx.drawImage(document.getElementById('background'), 0, 0, w, h);
+			ctx.globalAlpha = (1/z)/4;
+			ctx.drawImage(document.getElementById('nebula'), 0, 0, w, h);
+			ctx.globalAlpha = 1.0;
+			for(var i=0; i<=300; ++i) {
+				drawLine(i*(cw/300), 0, i*(cw/300), ch, "rgb(50,50,50)");
 			}
-			for(var i=0; i<=10; ++i) {
-				drawLine(0, i*(ch/10), cw, i*(ch/10), "rgb(50,50,50)");
+			for(var i=0; i<=300; ++i) {
+				drawLine(0, i*(ch/300), cw, i*(ch/300), "rgb(50,50,50)");
 			}
-			// drawLine(-50, -50, 50, 50, "rgb(128,128,128)");
 		}
 
 		function drawRect(x1, y1, x2, y2, colour){
 			ctx.fillStyle = colour;
 			ctx.fillRect(x(x1), y(y1), x(x2), y(y2));
 		}
-
 		function drawLine(x1, y1, x2, y2, colour){
 			ctx.strokeStyle = colour;
 			ctx.lineWidth = 1;
