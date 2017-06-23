2c8a284688082f6c32bd593c8f48e357415b2bc1
ophereon <ophereon@outlook.com>
2017-06-24 08:24:15 +1200

    24/06/17 - 08:23

--- map.js
+++ map.js
@@ -23,6 +23,10 @@ $(document).ready(function() {
 			startX = e.originalEvent.clientX;
 			startY = e.originalEvent.clientY;
 		}
+		console.clear();
+		console.log("px: "+e.originalEvent.clientX+", py: "+e.originalEvent.clientY);
+		console.log("cx: "+x(e.originalEvent.clientX)+", cy: "+y(e.originalEvent.clientY));
+		console.log("gx: "+gx+", gy: "+gy);
      })
     .mouseup(function(){
         press = false;
@@ -31,37 +35,42 @@ $(document).ready(function() {
 		startX = event.originalEvent.clientX;
 		startY = event.originalEvent.clientY;
 	    if (event.originalEvent.wheelDelta > 0 || event.originalEvent.detail < 0) {
-	        zoom(z + 0.1, startX, startY);
+	        zoom(z + z*0.1, startX, startY);
 	    }
 	    else {
-	        zoom(z - 0.1, startX, startY);
+	        zoom(z - z*0.1, startX, startY);
 	    }
 	});
 	window.onresize = function(){ resize(); }
 	$('body').css('top', -(document.documentElement.scrollTop) + 'px').addClass('noscroll');
 
 	if(canvas.getContext){
-		var ctx = canvas.getContext('2d');
-		var cw = 1000, ch = 1000;
-		var w = 0, h = 0;
-		var ox = 0, oy = 0;
-		var z = 1;
-		var m = 1;
+		var ctx = canvas.getContext('2d'); //canvas context
+		var cw = 1000, ch = 1000; //canvas size
+		var w = 0, h = 0; //width and height of window
+		var ox = 0, oy = 0; //offset x and offset y
+		var z = 1; //zoom level
+		var m = 1; //multiplier
+		var mz = 20; //max-zoom
+		var gx = 0; //
+		var gy = 0; //
 
 		function setup(){
 			resize();
 			zoom(1, 0, 0);
 			offsetX(0);
 			offsetY(0);
+			gx = x(0)-((w-cw)/2);
+			gy = y(0)-((h-ch)/2);
 			loop();
 		}
 		function resize(){
 			ctx.clearRect(0, 0, w, h);
 			w = canvas.width = window.innerWidth;
 			h = canvas.height = window.innerHeight;
-			var difW = cw/w;
-			var difH = ch/h;
-			m = Math.max(difW, difH);
+			var dw = cw/w;
+			var dh = ch/h;
+			m = Math.max(dw, dh);
 			draw();
 		}
 		function loop(){
@@ -71,7 +80,7 @@ $(document).ready(function() {
 
 		function zoom(level, posX, posY){
 			if(level<1) z = 1;
-			else if(level>10) z = 10;
+			else if(level>mz) z = mz;
 			else{
 				var difX = (ox+(posX/z)) - ((ox+(posX/level)));
 				var difY = (oy+(posY/z)) - ((oy+(posY/level)));
@@ -82,14 +91,12 @@ $(document).ready(function() {
 			// console.log("l: " + level);
 		}
 		function offsetX(inX){
-			// console.log(inX);
 			if(inX<-(w-(w/z))) ox = -(w-(w/z));
 			else if(inX>0) ox = 0;
 			else ox = inX;
 			if(cw*z<w) ox += (w-(cw*z))/2;
 		}
 		function offsetY(inY){
-			// console.log(inY);
 			if(inY<-(ch-(ch/z))) oy = -(ch-(ch/z));
 			else if(inY>0) oy = 0;
 			else oy = inY;
@@ -97,22 +104,20 @@ $(document).ready(function() {
 		}
 		function x(inX){ return ((inX+ox)*z)/m; }
 		function y(inY){ return ((inY+oy)*z)/m; }
+		function x2(inX){ return ((inX+ox)*((z/2)+0.5))/m; }
+		function y2(inY){ return ((inY+oy)*((z/2)+0.5))/m; }
 
 		function draw(){
 			ctx.clearRect(0, 0, w, h);
 			ctx.fillStyle = "rgb(0,0,0)";
 			ctx.fillRect(0, 0, w, h);
 			ctx.globalAlpha = 0.5;
-			ctx.drawImage(document.getElementById('background'), 0, 0, w, h);
-			ctx.globalAlpha = (1/z)/4;
-			ctx.drawImage(document.getElementById('nebula'), 0, 0, w, h);
+			ctx.drawImage(document.getElementById('background'), (w/2)-h, 0, (w/2)+h, h);
+			ctx.globalAlpha = (1-(z/mz))/2;
+			ctx.drawImage(document.getElementById('nebula'), (w/2)-h, 0, (w/2)+h, h);//x(0)-gx, y(0)-gy, x(cw)+gx, y(ch)+gy);
 			ctx.globalAlpha = 1.0;
-			for(var i=0; i<=300; ++i) {
-				drawLine(i*(cw/300), 0, i*(cw/300), ch, "rgb(50,50,50)");
-			}
-			for(var i=0; i<=300; ++i) {
-				drawLine(0, i*(ch/300), cw, i*(ch/300), "rgb(50,50,50)");
-			}
+			for(var i=0; i<=60; ++i) drawLine(i*(cw/60), 0, i*(cw/60), ch, "rgb(50,50,50)");
+			for(var i=0; i<=60; ++i) drawLine(0, i*(ch/60), cw, i*(ch/60), "rgb(50,50,50)");
 		}
 
 		function drawRect(x1, y1, x2, y2, colour){
@@ -129,6 +134,5 @@ $(document).ready(function() {
 			ctx.stroke();
 		}
 	}
-
 	setup();
 });
