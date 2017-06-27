d5cbb3a8f5fa14215eb0a4c305185082ad2adfd1
ophereon <ophereon@outlook.com>
2017-06-27 22:00:11 +1200

    27/06/17 - 22:00

--- map.js
+++ map.js
@@ -21,10 +21,10 @@ $(document).ready(function() {
 			startX = e.originalEvent.clientX;
 			startY = e.originalEvent.clientY;
 		}
-		// console.clear();
-		// console.log("px: "+e.originalEvent.clientX+", py: "+e.originalEvent.clientY);
-		// console.log("cx: "+x(e.originalEvent.clientX)+", cy: "+y(e.originalEvent.clientY));
-		// console.log("gx: "+gx+", gy: "+gy);
+		console.clear();
+		console.log("px: "+e.originalEvent.clientX+", py: "+e.originalEvent.clientY);
+		console.log("cx: "+x(e.originalEvent.clientX)+", cy: "+y(e.originalEvent.clientY));
+		console.log("gx: "+gx+", gy: "+gy);
      })
     .mouseup(function(){
         press = false;
@@ -44,7 +44,8 @@ $(document).ready(function() {
 
 	if(canvas.getContext){
 		var ctx = canvas.getContext('2d'); //canvas context
-		var cw = 1000, ch = 1000; //canvas size
+		var c = 500
+		var cw = c*2, ch = c*2; //canvas size
 		var w = 0, h = 0; //width and height of window
 		var ox = 0, oy = 0; //offset x and offset y
 		var z = 1; //zoom level
@@ -59,8 +60,8 @@ $(document).ready(function() {
 			zoom(1, 0, 0);
 			offsetX(0);
 			offsetY(0);
-			gx = x(0)-((w-cw)/2);
-			gy = y(0)-((h-ch)/2);
+			gx = x(0);
+			gy = y(0);
 			$.ajax({
         		type: "GET", url: "systems.csv", dataType: "text",
 		        success: function(file){ read(file); }
@@ -123,8 +124,8 @@ $(document).ready(function() {
 			else oy = inY;
 			if(ch*z<h) oy += (h-(ch*z))/2;
 		}
-		function x(inX){ return ((inX+ox)*z)/m; }
-		function y(inY){ return ((inY+oy)*z)/m; }
+		function x(inX){ return (((inX+ox)*z)/m)+(c); }
+		function y(inY){ return (((inY+oy)*z)/m)+(c); }
 		function r(inR){ return ((inR)*z)/m; }
 		function x2(inX){ return ((inX+ox)*((z/2)+0.5))/m; }
 		function y2(inY){ return ((inY+oy)*((z/2)+0.5))/m; }
@@ -136,15 +137,18 @@ $(document).ready(function() {
 			ctx.globalAlpha = 0.5;
 			ctx.drawImage(document.getElementById('background'), (w/2)-h, 0, (w/2)+h, h);
 			ctx.globalAlpha = (1-(z/mz))/2;
-			ctx.drawImage(document.getElementById('nebula'), (w/2)-h, 0, (w/2)+h, h); //x(0)-gx, y(0)-gy, x(cw)+gx, y(ch)+gy);
+			ctx.drawImage(document.getElementById('nebula'), (w/2)-h, 0, (w/2)+h, h); //x(0), y(0), x(cw), y(ch));
+			drawRect(-c, -c, c, c, 2, "rgb(255,255,255)");
+			drawLine(-c, -c, c, c, 2, "rgb(255,255,255)");
+			drawLine(c, -c, -c, c, 2, "rgb(255,255,255)");
 			ctx.globalAlpha = 1.0;
-			// for(var i=0; i<=60; ++i) drawLine(i*(cw/60), 0, i*(cw/60), ch, 0, "rgb(50,50,50)");
-			// for(var i=0; i<=60; ++i) drawLine(0, i*(ch/60), cw, i*(ch/60), 0, "rgb(50,50,50)");
+			// for(var i=0; i<=60; ++i) drawLine(i*(cw/60), 0, i*(cw/60), ch, 1, "rgb(50,50,50)");
+			// for(var i=0; i<=60; ++i) drawLine(0, i*(ch/60), cw, i*(ch/60), 1, "rgb(50,50,50)");
 			drawSystems();
 		}
 
 		function drawSystems(){
-			drawCirc(cw/2, ch/2, 10, 0, "rgb(0,0,0)");
+			drawCirc(0, 0, 10, 0, "rgb(0,0,0)");
 			for(var i=0; i<systems.length; ++i){
 				var colour = "rgb(0,0,0)";
 				switch(systems[i].type){
@@ -172,10 +176,14 @@ $(document).ready(function() {
 			ctx.fillStyle = colour;
 			ctx.strokeStyle = colour;
 			ctx.lineWidth = stroke;
-			var rectangle = new Path2D();
-    		rectangle.rect(x(x1), y(y1), x(x2), y(y2));
-			if(stroke==0) ctx.fill(rectangle);
-			else ctx.stroke(rectangle);
+			ctx.beginPath();
+			ctx.moveTo(x(x1), y(y1));
+			ctx.lineTo(x(x1), y(y2));
+			ctx.lineTo(x(x2), y(y2));
+			ctx.lineTo(x(x2), y(y1));
+			ctx.lineTo(x(x1), y(y1));
+			if(stroke==0) ctx.fill();
+			else ctx.stroke();
 		}
 		function drawCirc(x1, y1, r1, stroke, colour){
 			// console.log(x1+", "+y1+", "+r1+", "+stroke+", "+colour);
